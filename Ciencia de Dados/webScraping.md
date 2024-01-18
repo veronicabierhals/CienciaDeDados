02b25f6403e6692d4a8ab2c95b7837ece486a2f3
Veronica Bierhals <veronicabierhals1@gmail.com>
2024-01-18 09:27:52 -0300

    estudando web scraping

---
+++ Ciencia de Dados/webScraping.md
@@ -0,0 +1,17 @@
+# Web Scraping
+
+* Coletar dados automaticamente através de meios que não sejam uma API.
+* Aplicação capaz de consultar um servidor web, solicitar dados, analisar e extrair as informações necessárias.
+    * Extrair dados que estão "soltos" em uma página web.
+* É uma solução quando não há outras maneiras de coletar dados necessários a um projeto ou acelerar um processo que antes era feito manualmente.
+
+
+## Biblioteca beautifulSoup
+
+```
+pip install bs4
+```
+
+* Constrói uma árvore a partir de vários elementos de uma página e fornece uma interface simples para acessá-los.
+* Utilizada junto com a biblioteca Requests e de um parseador html.
+* Outras opções são a Selenium e Scrapy que tem uma lógica de pensamento para operá-las similar.
---
+++ Codigos/bibliotecaBeautifulsoup.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+from unidecode import unidecode
+import pandas as pd
+
+html = requests.get(
+    "https://statisticstimes.com/tech/top-computer-languages.php").content
+soup = BeautifulSoup(html, 'html5lib')
+
+# find busca o primeiro que ele achar
+primeiro_paragrafo = soup.find('p')
+# print(primeiro_paragrafo) # aparece com as tags
+# print(primeiro_paragrafo.text) # aparece somente texto
+
+# find_all busca tudo que tem e joga em uma lista
+todos_paragrafos = soup.find_all('p')
+# print(todos_paragrafos) # lista todos os parágrafos
+# print(todos_paragrafos[0].text) # imprime somente o primeiro parágrafo
+
+todos_links = soup.find_all('a')
+# print(todos_links)
+
+'''
+abre o site e busca no código HTML dele o nome da informação que deseja coletar
+'''
+# busca table com nome table_id na parte de tbody (todas essas infos são retiradas do site)
+tabela = soup.find('table', {'id': 'table_id1'}).find('tbody')
+print(tabela)
+
+linhas=tabela.find_all('tr')
+for linha in linhas:
+    dados = linha.find_all('td')
+    print(dados[0].text)
+    print(dados[2].text)
+    print(dados[3].text)
+    print('------')
+
+# continua no arquivo Colab
